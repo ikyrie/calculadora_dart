02c255467b5ebec5b7ef5582d17803539e89c68f
ikyrie <mah.c5r@gmail.com>
2023-04-10 17:05:08 -0300

    Criando os testes para a calculadora

--- test/calculadora_dart_test.dart
+++ test/calculadora_dart_test.dart
@@ -2,5 +2,25 @@ import 'package:calculadora_dart/calculadora_dart.dart';
 import 'package:test/test.dart';
 
 void main() {
-  
+  const int a = 10;
+  const int b = 5;
+  test("Testa se a função de soma faz o cálculo correto.", () {
+    final resultado = Calculadora.adicao(a, b);
+    expect(resultado, 15);
+  });
+
+  test("Testa se a função de subtrair faz o cálculo correto", () {
+    final resultado = Calculadora.subtracao(a, b);
+    expect(resultado, 5);
+  });
+
+  test("Testa se a função de multiplicação faz o cálculo correto", () {
+    final resultado = Calculadora.multiplicacao(a, b);
+    expect(resultado, 50);
+  });
+
+  test("Testa se a função de divisão faz o cálculo correto", () {
+    final resultado = Calculadora.divisao(a, b);
+    expect(resultado, 2);
+  });
 }
